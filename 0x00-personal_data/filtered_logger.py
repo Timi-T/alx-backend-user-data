4f7c84e6cefa5d6880ce26a266235754068d8e46
Timi-T <timiopebode@gmail.com>
2022-07-21 09:21:07 +0100

    pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Module to obfuscate logs to protect person data
 """
 
 
+import logging
 import re
 from typing import List
 
@@ -24,3 +25,19 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         pat = field + '=[^{}]*'.format(separator)
         message = re.sub(pat, field + '=' + redaction, message)
     return(message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
