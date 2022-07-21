cfc9f86ab3186c21fdf4ab5694cfd90ddb86222c
Timi-T <timiopebode@gmail.com>
2022-07-21 14:30:48 +0100

    change pii fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ import re
 from typing import List
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
