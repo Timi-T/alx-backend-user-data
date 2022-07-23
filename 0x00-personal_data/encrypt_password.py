79161e42631ee282ccd817e5677729a1f7b8913f
Timi-T <timiopebode@gmail.com>
2022-07-23 15:26:24 +0100

    bcrypt hashing

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/bash
+""" Hash a password with bcrypt package"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Function to has a password"""
+    salt = bcrypt.gensalt()
+    hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_pw
