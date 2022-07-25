afc8809253fd57973b242484c4e85d5ae24cfd8e
Timi-T <timiopebode@gmail.com>
2022-07-25 14:00:10 +0100

    use auth object for authorization

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -12,6 +12,26 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def authenticate():
+    """Authenticate a request if necessary"""
+    if auth:
+        requires_auth = auth.require_auth(request.path, [
+                                            '/api/v1/status/',
+                                            '/api/v1/unauthorized/',
+                                            '/api/v1/forbidden/'
+                                        ])
+        if requires_auth:
+            if not auth.authorization_header(request):
+                return abort(401)
+            if not auth.current_user(request):
+                return abort(403)
 
 
 @app.errorhandler(404)
