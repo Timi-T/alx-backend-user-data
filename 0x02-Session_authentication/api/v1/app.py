76272e2b16e2bd67ce1422e918b1b91f672b05a8
Timi-T <timiopebode@gmail.com>
2022-07-27 15:56:40 +0100

    exclude login path from paths that require login

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,10 +32,12 @@ def authenticate():
                                             '/api/v1/status/',
                                             '/api/v1/unauthorized/',
                                             '/api/v1/forbidden/',
-                                            '/api/v1/stat*'
+                                            '/api/v1/stat*',
+                                            '/api/v1/auth_session/login/'
                                         ])
         if requires_auth:
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request)\
+                    and not auth.session_cookie(request):
                 return abort(401)
             current_user = auth.current_user(request)
             if not current_user:
