54c8f407c39c5559637f72e00a8ea590b5c27521
Timi-T <timiopebode@gmail.com>
2022-07-28 10:38:05 +0100

    load sessio from db

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -4,6 +4,9 @@ Stores a session for user
 """
 
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from datetime import datetime, timedelta
+from models.user_session import UserSession
+from models.base import Base
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -11,11 +14,26 @@ class SessionDBAuth(SessionExpAuth):
 
     def create_session(self, user_id=None):
         """Create and stores a data for each session"""
-        return super().create_session(user_id)
+        session_id = super().create_session(user_id)
+        session = UserSession(**{'user_id': user_id, 'session_id': session_id})
+        session.save()
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """Get a user id from database using session id"""
-        return super().user_id_for_session_id(session_id)
+        UserSession.load_from_file()
+        sessions = UserSession.search({'session_id': session_id})
+        if sessions:
+            session = sessions[0].to_json()
+            created = session.get('created_at')
+            created = datetime.strptime(created, "%Y-%m-%dT%H:%M:%S")
+            exp_date = created + timedelta(seconds=self.session_duration)
+            user_id = session.get('user_id')
+            if exp_date - datetime.now() < timedelta(hours=-1, seconds=0):
+                return None
+            else:
+                return user_id
+        return None
 
     def destroy_session(self, request=None):
         """Destroy a user session"""
