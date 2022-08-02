50ed347fa54e6d559ce874a7c9d6ec5c1c4d5bab
Timi-T <timiopebode@gmail.com>
2022-08-02 11:13:52 +0100

    when no arguments are passed

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,6 +45,8 @@ class DB:
 
     def find_user_by(self, **kwargs: Dict) -> User:
         """Method to find a user using keyword filters"""
+        if not kwargs:
+            raise NoResultFound
         attr = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
         for k, v in kwargs.items():
             if k not in attr:
